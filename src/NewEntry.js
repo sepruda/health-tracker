f31dfb4a1c246cb0d5f50f15f69fa61e240750a3
Christian Jess Mark <christianjessmark@gmail.com>
2020-08-03 17:43:11 +0200

    chore: add styled components

--- src/NewEntry.js
+++ src/NewEntry.js
@@ -1,10 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+`;
+
+const Card = styled.div`
+    width: 400px;
+    border: 1px solid grey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 function NewEntry() {
     return (
-        <div>
-            <h2>{Date.now()}</h2>
-        </div>
+        <Wrapper>
+            <Card>
+                <h2>Date: {Date.now()}</h2>
+            </Card>
+        </Wrapper>
     );
 }
 
