c44699c94d269eb2ac2b0caa625a20b12433b18c
Christian Jess Mark <cjess@dfds.com>
2020-08-14 15:30:30 +0200

    feat: new UI and firebase deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from "react";
-import "./App.css";
-import NewEntry from "./NewEntry";
+import React from 'react'
+import MomentUtils from '@date-io/moment'
+import { StylesProvider } from '@material-ui/core'
+import { MuiPickersUtilsProvider } from '@material-ui/pickers'
+import moment from 'moment'
+
+import 'moment/locale/da'
+
+import NewEntry from './NewEntry'
+
+moment.locales('da')
 
 function App() {
-    return (
-        <div className="App">
-            <NewEntry />
-        </div>
-    );
+  return (
+    <StylesProvider injectFirst>
+      <MuiPickersUtilsProvider utils={MomentUtils} locale="da">
+        <NewEntry />
+      </MuiPickersUtilsProvider>
+    </StylesProvider>
+  )
 }
 
-export default App;
+export default App
--- src/NewEntry.js
+++ src/NewEntry.js
@@ -1,30 +1,117 @@
-import React from "react";
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    height: 100vh;
-`;
-
-const Card = styled.div`
-    width: 400px;
-    border: 1px solid grey;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
+/** @jsx jsx */
+import React, { useState } from 'react'
+import { css, jsx } from '@emotion/core'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Slider,
+  Typography,
+} from '@material-ui/core'
+import { DatePicker } from '@material-ui/pickers'
 
 function NewEntry() {
-    return (
-        <Wrapper>
-            <Card>
-                <h2>Date: {Date.now()}</h2>
-            </Card>
-        </Wrapper>
-    );
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date)
+  }
+
+  const handleOnClick = (e) => {
+    e.preventDefault()
+    console.log('Button clicked')
+  }
+
+  return (
+    <div
+      css={css`
+        padding: 0 20px;
+      `}
+    >
+      <Card
+        css={css`
+          min-width: 275px;
+          max-width: 350px;
+          background-color: lavenderblush;
+        `}
+      >
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Dagsform
+          </Typography>
+          <DatePicker
+            disableFuture
+            format="dddd - DD. MMMM"
+            margin="normal"
+            label="dato"
+            id="date-picker"
+            value={selectedDate}
+            onChange={handleDateChange}
+          />
+          <div>
+            <Typography id="sleep-slider" gutterBottom>
+              Søvn
+            </Typography>
+            <Slider
+              defaultValue={5}
+              aria-labelledby="sleep-slider"
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={0}
+              max={10}
+            />
+          </div>
+          <div>
+            <Typography id="arms-slider" gutterBottom>
+              Arme
+            </Typography>
+            <Slider
+              defaultValue={5}
+              aria-labelledby="arms-slider"
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={0}
+              max={10}
+            />
+          </div>
+          <div>
+            <Typography id="mood-slider" gutterBottom>
+              Humør
+            </Typography>
+            <Slider
+              defaultValue={5}
+              aria-labelledby="mood-slider"
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={0}
+              max={10}
+            />
+          </div>
+          <div>
+            <Typography id="screen-slider" gutterBottom>
+              Computer/Telefon
+            </Typography>
+            <Slider
+              defaultValue={5}
+              aria-labelledby="screen-slider"
+              valueLabelDisplay="auto"
+              step={1}
+              marks
+              min={0}
+              max={10}
+            />
+          </div>
+        </CardContent>
+        <CardActions>
+          <Button onClick={handleOnClick}>Gem</Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
 }
 
-export default NewEntry;
+export default NewEntry
